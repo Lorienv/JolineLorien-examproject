834cbed990697b8c67ce01ca5283429a56620c60
Lorienv <lorien.verachtert@hotmail.com>
2017-01-02 20:15:00 +0100

    I finished the corpus we will use for topic modeling

--- Textanalyses_of_1001_nights.py
+++ Textanalyses_of_1001_nights.py
@@ -337,7 +337,7 @@ sentence_dic_nights = {}
 for file in corpus_nightsII.fileids(): #calculate the amount of sentences in each volume
 	sentence_list_nights.append((file, len(corpus_nightsII.sents(file))))
 	sentence_dic_nights[file] = len(corpus_nightsII.sents(file))
-	sentence_dic_nights = collections.OrderedDict(sentence_dic_nights) #we make sure that the order of the data stays the same'''
+	sentence_dic_nights = collections.OrderedDict(sentence_dic_nights) #we make sure that the order of the data stays the same
 
 #####################################
 #visualise statistics for each night
@@ -345,7 +345,7 @@ for file in corpus_nightsII.fileids(): #calculate the amount of sentences in eac
 
 #collect data for table with total numbers for each night
 
-characters_per_night = [] #list of the characters per night
+'''characters_per_night = [] #list of the characters per night
 for tuples in char_list_night:
 	characters_per_night.append(tuples[1])	
 print(characters_per_night)
@@ -385,10 +385,10 @@ df = pd.DataFrame({'Nights': column1,'Total characters': column2,'Total lines': 
 from pandas import ExcelWriter as xlwt #import excelwriter module
 from xlwt import Workbook
 writer = xlwt('table of all nights.xlsx') #create an excel file from the data frame
-workbook = writer.book
+workbook = writer.book #define the excel workbook
 df.to_excel(writer, 'Sheet1') #place the data frame on the first sheet of the excel file
-worksheet = writer.sheets['Sheet1'] #define a worksheet
-worksheet.set_column('B:F',35) #set the column width for columns B up to F
+worksheet = writer.sheets['Sheet1'] #define the worksheet
+worksheet.set_column('B:F',35) #set the column width for columns B up to F, so we can see all the text in the cells
 format = workbook.add_format({'bold': True, 'font_color': 'red'}) #put in bold and red the night with the highest number of char., lines, sents. and words
 
 #find the highest numbers per column (per list)
@@ -404,17 +404,19 @@ worksheet.write('F31', '11794' , format) #highlight the night with the highest n
 worksheet.write('B31', 'the Eight Hundred and Forty-fifth', format)#highlight the name of the night with the highest numbers
 writer.save() #save and close the excel file
 
+#now the general visualisations of the statistics are finished, we can start preparing the texts for topic modeling '''
+
 #######################################
-#Prepare the texts for topic modelling
+#Prepare the texts for topic modeling
 #######################################
 # First, we make a new corpus consisting of the nights and some additional texts, because we need enough data to apply topic modelling.
 
-pattern = re.compile(r'[Tt]he\s') #Lorien, hier zal dus nog wat achter komen, afhankelijk van de namen van die andere sprookjes.
+pattern = re.compile(r'[Tt]he') #Lorien, hier zal dus nog wat achter komen, afhankelijk van de namen van die andere sprookjes.
 corpus_tales = []
 for file in listdir('data'):
 	if pattern.search(file):
 		corpus_tales.append('data' + '/' + file)
-print(len(corpus_tales)) #990 files are in the corpus (voorlopig)
+print(len(corpus_tales)) #for topic modeling we should have a minimum of 1000 files, now we have 1030 files
 
 #Now that we have our corpus, we need to tokenize every file so we can leave out the punctuation, stopwords and save them in 'clean_doc'.
 import string
